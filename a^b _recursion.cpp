7663c64edc0cface8333f286277273dc92cba8da
MRIDUL SHARMA <108065487+mridulsharma000@users.noreply.github.com>
2022-06-24 17:32:58 +0530

    Update a^b _recursion.cpp

--- a^b _recursion.cpp
+++ a^b _recursion.cpp
@@ -17,3 +17,42 @@ int main() {
     cout<<ans;
 	return 0;
 }
+
+
+//MORE OPTIMIZED APPROACH
+
+#include<iostream>
+using namespace std;
+
+int power(int a, int b) {
+    //base case
+    if( b == 0 )
+        return 1;
+
+    if(b == 1)
+        return a;
+
+    //RECURSIVE CALL
+    int ans = power(a, b/2);
+
+    //if b is even
+    if(b%2 == 0) {
+        return ans * ans;  //even toh return (a^ b/2)* (a^ b/2)
+    }
+    else {
+        //if b is odd
+        return a * ans * ans; //odd toh return a* (a^ b/2)* (a^ b/2)
+    }
+}
+
+int main() {
+
+    int a,b;
+    cin >> a >> b;
+    cout << endl;
+    int ans = power(a,b);
+
+    cout << "Answer is " << ans << endl;
+
+    return 0;
+}
