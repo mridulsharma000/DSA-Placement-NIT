43ea2124baacdb910f5206d99294bbbdeacfc64c
MRIDUL SHARMA <108065487+mridulsharma000@users.noreply.github.com>
2022-07-08 18:11:37 +0530

    Create LL_All_InsertionCodes.cpp

---
+++ LL_All_InsertionCodes.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    
+    node(int data){
+        this-> data=data;
+        this-> next=NULL;
+    }
+};
+
+
+void insertAtHead(node* &head, int d){
+    node* temp= new node(d);//ye wala ghusana hai
+    
+        temp->next=head;
+        head= temp;
+    
+}
+
+
+void insertAtTail(node* &tail, int d){
+    node* temp= new node(d);
+    tail-> next=temp;
+    tail= tail->next;
+    
+}
+
+
+void insertAtAny(int position, node* &head, node* &tail, int d){
+  
+    if(position==1){
+        insertAtHead(head,d);
+        return;
+    }
+    
+    
+    node*first =head;
+    if(first->next==NULL){
+        insertAtTail(tail,d);
+        return;
+    }
+    
+    int cnt=1;
+    while(cnt<position-1){
+        first=first->next;
+        cnt++;
+    }
+    
+    node*temp= new node(d);//node to insert
+    temp->next= first->next;
+    first->next=temp;
+}
+
+void Print(node* &head){
+    node* temp= head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+bool Search(node* &head, int key){
+    node* temp= head;
+    while(temp!=NULL){
+        if(temp->data==key){
+            return true;
+        }
+        temp= temp-> next;
+    }
+    return false;
+}
+
+int main(){
+    node* a= new node(78);
+    node* head= a;
+    node* tail= a;
+    insertAtHead(head,16);
+    insertAtTail(tail,19);
+    Print(head);
+    insertAtAny(4,head,tail,31);
+    insertAtAny(2,head,tail,2023);
+    Print(head);
+    insertAtAny(1,head,tail,576);
+    Print(head);
+    cout<<Search(head, 78);
+}
